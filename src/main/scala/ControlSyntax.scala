3a5130c4f32443725ecc2fde61ca4aadce144e27
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-26 19:58:10 +0900

    [update]for式について学習

--- src/main/scala/ControlSyntax.scala
+++ src/main/scala/ControlSyntax.scala
@@ -16,4 +16,10 @@ object ControlSyntax {
     val result = while (i < 10) i = i + 1
     println(result)
   }
+
+  def doubleLoop(): Unit = {
+    for (x <- 1 to 5; y <- 1 until 5) {
+      println(s"x = ${x} y = ${y}")
+    }
+  }
 }
