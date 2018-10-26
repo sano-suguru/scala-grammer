7d6c8048a691fe8697511c46f7f7ca330933ab38
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-26 20:02:28 +0900

    [update]コメントを追加

--- src/main/scala/ControlSyntax.scala
+++ src/main/scala/ControlSyntax.scala
@@ -1,7 +1,11 @@
 object ControlSyntax {
+  // 式はセミコロンで区切る{ exp1; exp2; ... expN; }
+  // 式なので最後に評価されたものが戻り値
   def bar(): String = {
     "bar"
   }
+
+  // scalaにおいてifは式
   def printOver18(age: Int): Unit = {
     val message = if (age < 18) {
       "18歳未満です"
@@ -11,12 +15,15 @@ object ControlSyntax {
     println(message)
   }
 
+  // whileも式
   def printWhileResult(): Unit = {
     var i = 0
     val result = while (i < 10) i = i + 1
     println(result)
   }
 
+  // forも式
+  // 多重ループをひとつの for で表現で書ける
   def doubleLoop(): Unit = {
     for (x <- 1 to 5; y <- 1 until 5) {
       println(s"x = ${x} y = ${y}")
