e722e3a9507b63468325030c393621314c2bc0b3
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-26 19:42:51 +0900

    if式について学習

--- src/main/scala/ControlSyntax.scala
+++ src/main/scala/ControlSyntax.scala
@@ -1,3 +1,13 @@
 object ControlSyntax {
-
+  def bar(): String = {
+    "bar"
+  }
+  def printOver18(age: Int): Unit = {
+    val message = if (age < 18) {
+      "18歳未満です"
+    } else {
+      "18歳以上です"
+    }
+    println(message)
+  }
 }
