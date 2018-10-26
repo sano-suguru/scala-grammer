6fb1dafcc9f6d5ff7f77089591576dc2c42986b7
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-26 20:31:15 +0900

    [update]

--- src/main/scala/ControlSyntax.scala
+++ src/main/scala/ControlSyntax.scala
@@ -54,4 +54,6 @@ object ControlSyntax {
     } while (i < 10)
   }
 
+  def printRightTriangles(): Unit =
+    for (a <- 1 to 1000; b <- 1 to 1000; c <- 1 to 1000 if a * a == b * b + c * c) println((a, b, c))
 }
