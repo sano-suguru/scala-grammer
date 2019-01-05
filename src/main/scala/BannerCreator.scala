21b75851fa4be8f0c288af399abf520225b3c616
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2019-01-05 14:56:08 +0900

    オブジェクトの使い方

---
+++ src/main/scala/BannerCreator.scala
@@ -0,0 +1,13 @@
+object BannerCreator {
+  private val decor = ".｡:･・.｡:*･★.｡:･・.｡:*･★.｡:･・.｡:*･★.｡:･・.｡:*･★.｡:･・.｡:*･★"
+
+  def create(message: String) = {
+    s"""$decor
+       |$message
+       |$decor""".stripMargin
+  }
+}
+
+object MessageContainer {
+  var message = ""
+}
