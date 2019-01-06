122217e1e021d3374d76e8f8284657e52f3e8f98
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2019-01-06 17:19:11 +0900

    線形化

--- src/main/scala/PaperCup.scala
+++ src/main/scala/PaperCup.scala
@@ -1,3 +1,32 @@
-class PaperCup {
-
+trait  Fillable {
+  def fill(): Unit = println("fill!")
 }
+
+trait  Disposable {
+  def dispose(): Unit = println("dispose!")
+}
+
+class Paper
+
+class PaperCup extends Paper with Fillable with Disposable
+
+trait TraitA {
+  def greet(): Unit = println("Hello!")
+}
+
+trait TraitB extends TraitA {
+  override def greet(): Unit = {
+    super.greet()
+    println("My name is Terebi-chan.")
+  }
+}
+
+trait TraitC extends TraitA {
+  override def greet(): Unit = {
+    super.greet()
+    println("I like niconico.")
+  }
+}
+
+class Class1 extends TraitB with TraitC
+class Class2 extends TraitC with TraitB
